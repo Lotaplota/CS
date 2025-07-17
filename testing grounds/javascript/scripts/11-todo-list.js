f85b517dafdad02e912fa5db01cba888276f20c8
lotaplota <kristhian.lucas@gmail.com>
2025-07-17 19:30:30 -0300

    still need to finish the list generator

--- testing grounds/javascript/scripts/11-todo-list.js
+++ testing grounds/javascript/scripts/11-todo-list.js
@@ -1,21 +1,54 @@
-let todoList = [];
+let todoList = [{
+  name: 'finish this course',
+  dueDate: '03-08-2025'
+}, {
+  name: 'finish the wiki site',
+  dueDate: '03-08-2025'
+}];
 let todoListHTML = '';
 
+for (let i = 0; i < todoList.length; i++) {
+  console.log(todoList[i]);
+}
+
 function addTodo() {
-  let inputLm = document.querySelector('.js-add-todo-field');
-  const todoText = inputLm.value;
-  inputLm.value = '';
-  todoList.push(todoText);
+  const textInputLm = document.querySelector('.js-add-todo-field');
+  const todoText = textInputLm.value;
+  textInputLm.value = '';
+
+  const dateInputLm = document.querySelector('.js-todo-due-date-field');
+  const todoDueDate = dateInputLm.value;
+  dateInputLm.value = '';
+
+  todoList.push({name: todoText, dueDate: todoDueDate});
   
-  updateTodoListHTML(todoText);
-  sendToPage();
+  renderTodoList();
 }
 
-function updateTodoListHTML(text) {
-  todoListHTML = todoListHTML + `<p>${text}</p>`;
+function generateListHTML() {
+  todoListHTML = '';
+
+  for (let i = 0; i < todoList.length; i++)
+  {
+    const todo = todoList[i];
+    todoListHTML += `
+      <p>
+        ${todo.name}, ${todo.dueDate}
+        <button onclick="deleteTodo(${i});">
+          Delete
+        </button>
+      </p>`;
+  }
 }
 
-function sendToPage() {
+function deleteTodo(i) {
+  todoList.splice(i, 1);
+  renderTodoList();
+}
+
+function renderTodoList() {
+  generateListHTML();
+
   const listLm = document.querySelector('.todo-list-div');
   listLm.innerHTML = todoListHTML;
 }
