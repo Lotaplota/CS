e7ccef02943c106c0249c362e51449c5b793f5fc
lotaplota <kristhian.lucas@gmail.com>
2025-07-25 17:21:53 -0300

    now the website handles localStorage

--- testing grounds/javascript/javascript-amazon-project-main/data/cart.js
+++ testing grounds/javascript/javascript-amazon-project-main/data/cart.js
@@ -1,10 +1,24 @@
-export let cart = [{
-  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity: 2
-}, {
-  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1
-}];
+export let cart = JSON.parse(localStorage.getItem('cart')) || [{ // Why didn't the teacher use the default operator like this?
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 666
+    }, {
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 1
+    }];
+
+// if (!cart) {
+//   cart = [{
+//     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+//     quantity: 666
+//     }, {
+//       productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+//       quantity: 1
+//     }];
+// }
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
   let matchingItem;
@@ -23,6 +37,8 @@ export function addToCart(productId) {
       quantity: 1
     })
   }
+  
+  saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -44,4 +60,6 @@ export function removeFromCart(productId) {
 
   cart = newCart;
   // Isn't this method more memory intensive?
+
+  saveToStorage();
 }
