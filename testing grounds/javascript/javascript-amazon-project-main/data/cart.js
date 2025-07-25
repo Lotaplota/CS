44cd9f736610d20e7dcd3da4a6d3ed3acc3f8164
lotaplota <kristhian.lucas@gmail.com>
2025-07-25 19:25:59 -0300

    minor changes

--- testing grounds/javascript/javascript-amazon-project-main/data/cart.js
+++ testing grounds/javascript/javascript-amazon-project-main/data/cart.js
@@ -1,10 +1,13 @@
-export let cart = JSON.parse(localStorage.getItem('cart')) || [{ // Why didn't the teacher use the default operator like this?
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 666
-    }, {
-      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-      quantity: 1
-    }];
+export let cart = JSON.parse(localStorage.getItem('cart')) ||
+[{ // Why didn't the teacher use the default operator like this?
+  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+  quantity: 666,
+  deliveryOptionId: 1
+}, {
+  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+  quantity: 1,
+  deliveryOptionId: 2
+}];
 
 // if (!cart) {
 //   cart = [{
