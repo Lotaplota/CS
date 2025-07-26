dfadabc6bc32f38790c88c4f1a42cef34e66d5d2
lotaplota <kristhian.lucas@gmail.com>
2025-07-26 12:52:03 -0300

    implemented the delivery options html. Still have to test it

--- testing grounds/javascript/javascript-amazon-project-main/scripts/checkout.js
+++ testing grounds/javascript/javascript-amazon-project-main/scripts/checkout.js
@@ -1,9 +1,10 @@
-import {cart, removeFromCart} from '../data/cart.js';
-import {products} from '../data/products.js';    // Double dots means return one folder
-import {formatCurrency} from './utils/money.js'; // Single dot means same folder
-import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
+import { cart, removeFromCart } from '../data/cart.js';
+import { products } from '../data/products.js';    // Double dots means return one folder
+import { formatCurrency } from './utils/money.js'; // Single dot means same folder
+import { hello } from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js'; // The ESM version of the code is needed to use it as a module (see ECMAScript)
                                                                                // This is also called a "default export". The other ones are called "named exports" 
+import { deliveryOptions } from '../data/deliveryOptions.js';
 
 let today = dayjs().format('dddd, MMM. D');
 console.log(today);
@@ -56,50 +57,45 @@ cart.forEach(cartItem => {
           <div class="delivery-options-title">
             Choose a delivery option:
           </div>
-          <div class="delivery-option">
-            <input type="radio"
-              class="delivery-option-input"
-              name="delivery-option-${matchingProduct.id}">
-            <div>
-              <div class="delivery-option-date">
-                Tuesday, June 21
-              </div>
-              <div class="delivery-option-price">
-                FREE Shipping
-              </div>
-            </div>
-          </div>
-          <div class="delivery-option">
-            <input type="radio"
-              class="delivery-option-input"
-              name="delivery-option-${matchingProduct.id}">
-            <div>
-              <div class="delivery-option-date">
-                Wednesday, June 15
-              </div>
-              <div class="delivery-option-price">
-                $4.99 - Shipping
-              </div>
-            </div>
-          </div>
-          <div class="delivery-option">
-            <input type="radio"
-              class="delivery-option-input"
-              name="delivery-option-${matchingProduct.id}">
-            <div>
-              <div class="delivery-option-date">
-                Monday, June 13
-              </div>
-              <div class="delivery-option-price">
-                $9.99 - Shipping
-              </div>
-            </div>
-          </div>
+          ${deliveryOptionsHTML()}
         </div>
       </div>
     </div>
   `
 })
+
+function deliveryOptionsHTML() {
+  let html = '';
+
+  deliveryOptions.forEach(deliveryOption => {
+    const today = dayjs();
+    const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
+    const dateString = deliveryDate.format('dddd, MMMM D')
+
+    const priceString = deliveryOption.priceCents
+      ? `${formatCurrency(deliveryOption.priceCents)} -`
+      : 'FREE'
+    ;
+
+    html += `
+      <div class="delivery-option">
+        <input type="radio"
+          class="delivery-option-input"
+          name="delivery-option-${matchingProduct.id}">
+        <div>
+          <div class="delivery-option-date">
+            ${dateString}
+          </div>
+          <div class="delivery-option-price">
+            ${priceString} Shipping
+          </div>
+        </div>
+      </div>
+    `
+  });
+
+  return html;
+}
 
 document.querySelector('.js-order-summary')
   .innerHTML = cartSummaryHTML;
