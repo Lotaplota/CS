3f0cc1125a434babf654e67de227bcdf6cd15599
lotaplota <kristhian.lucas@gmail.com>
2025-10-29 11:44:52 -0300

    dude I tried everything, but this jasmine bit simply doesn't work properly

--- study/javascript/javascript-amazon-project-main/data/cart.js
+++ study/javascript/javascript-amazon-project-main/data/cart.js
@@ -1,15 +1,15 @@
 import { getProduct } from "./products.js";
 
-export let cart = JSON.parse(localStorage.getItem('cart')) ||
-[{ // Why didn't the teacher use the default operator like this?
-  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity: 666,
-  deliveryOptionId: '1'
-}, {
-  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1,
-  deliveryOptionId: '2'
-}];
+// export let cart = JSON.parse(localStorage.getItem('cart')) ||
+// [{ // Why didn't the teacher use the default operator like this?
+//   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+//   quantity: 666,
+//   deliveryOptionId: '1'
+// }, {
+//   productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+//   quantity: 1,
+//   deliveryOptionId: '2'
+// }];
 
 // if (!cart) {
 //   cart = [{
@@ -20,6 +20,26 @@ export let cart = JSON.parse(localStorage.getItem('cart')) ||
 //       quantity: 1
 //     }];
 // }
+
+export let cart;
+
+loadFromStorage();
+
+export function loadFromStorage() {
+  cart = JSON.parse(localStorage.getItem('cart'));
+
+  if (!cart) {
+    cart = [{
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 2,
+      deliveryOptionId: '1'
+    }, {
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 1,
+      deliveryOptionId: '2'
+    }];
+  }
+}
 
 function saveToStorage() {
   localStorage.setItem('cart', JSON.stringify(cart));
--- study/javascript/javascript-amazon-project-main/scripts/checkout.js
+++ study/javascript/javascript-amazon-project-main/scripts/checkout.js
@@ -2,9 +2,9 @@ import { cart } from '../data/cart.js';
 import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 
-renderCheckout();
 renderOrderSummary();
 renderPaymentSummary();
+renderCheckout();
 
 export function renderCheckout(){
     document.querySelector('.return-to-home-link').innerHTML = cart.length;
